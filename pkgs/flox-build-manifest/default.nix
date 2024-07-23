4977e0256edd8334e1ef0cd438024c01408103e4
Tom Bereknyei <tomberek@gmail.com>
2024-07-23 19:52:24 -0400

    fix: easier limited running

--- pkgs/flox-build-manifest/default.nix
+++ pkgs/flox-build-manifest/default.nix
@@ -2,7 +2,7 @@
 writeShellApplication {
   name = "flox";
   text = ''
-set -eau
+set -eauo pipefail
 if [ -z "$FLOX_ENV" ]; then
   echo "FLOX_ENV not set, exiting."
   exit 1
@@ -11,20 +11,23 @@ fi
 case "''${1-unset}" in
   "build") FLOX_BUILD_COMMAND="build" ;;
   "make") FLOX_BUILD_COMMAND="make" ;;
+  "phase") FLOX_BUILD_COMMAND="phase" ;;
   *) exec "$FLOX_BIN" "$@" ;;
 esac
 shift
 
-set -x
 build_args=()
 nix_args=()
+args=()
+target=build_args
 while [[ "$#" -gt 0 ]]; do
      case "$1" in
-       --)                 build_args=("''${nix_args[@]}"); nix_args=(); ;;
-       *) nix_args=("$1" "''${nix_args[@]}");  ;;
+       --)  build_args=("''${args[@]}");  args=(); target="nix_args"; ;;
+       *)   args=("''${args[@]}" "$1");  ;;
      esac
      shift;
 done
+declare -a "$target"="''${args[*]}"
 
 pushd "$FLOX_ENV_PROJECT"
 
@@ -39,18 +42,26 @@ PREFIX="/tmp/store_$(readlink -f "$FLOX_ENV" | sha256sum | head -c32)-$name"
 export PREFIX
 export out="$PREFIX"
 
-# TODO: find a better place to put this, and/or clean up afterwards
-${dasel}/bin/dasel -w xml -f "$FLOX_ENV_CACHE"/../env/manifest.toml vars."$FLOX_BUILD_COMMAND"_command > "$FLOX_ENV_CACHE"/../build_script.sh
-chmod +x "$FLOX_ENV_CACHE"/../build_script.sh
-"$FLOX_ENV"/activate "$FLOX_ENV_CACHE"/../build_script.sh "''${build_args[@]}"
+# NOTE: build_args are not correctly passed by the activate script
+if [[ "''${FLOX_BUILD_COMMAND}" = "phase" ]]; then
+  "$FLOX_ENV"/activate -c "''${build_args[*]}"
+else
+  # TODO: find a better place to put this, and/or clean up afterwards
+  ${dasel}/bin/dasel -w xml -f "$FLOX_ENV_CACHE"/../env/manifest.toml vars."$FLOX_BUILD_COMMAND"_command > "$FLOX_ENV_CACHE"/../build_script.sh
+  chmod +x "$FLOX_ENV_CACHE"/../build_script.sh
+
+  "$FLOX_ENV"/activate "$FLOX_ENV_CACHE"/../build_script.sh
+fi
 
 # Create new env layering results of build script with original env.
 # Note: read name from manifest.toml (includes version)
-nix build --file ${./build-manifest.nix} \
-  --argstr name "$name" \
-  --argstr flox-env "$FLOX_ENV" \
-  --argstr install-prefix "$PREFIX" \
-  -L "''${nix_args[@]}"
+if [[ "''${FLOX_BUILD_COMMAND}" != "phase" ]]; then
+  nix build --file ${./build-manifest.nix} \
+    --argstr name "$name" \
+    --argstr flox-env "$FLOX_ENV" \
+    --argstr install-prefix "$PREFIX" \
+    -L "''${nix_args[@]}"
+fi
 
 popd
 
